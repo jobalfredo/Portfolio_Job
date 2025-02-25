ff286c5090765a0a1427b7bfa04a76ab73de568a
jobalfredo <jobalfredo@gmail.com>
2025-02-25 13:58:37 +0800

    updated files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from '../assets/Cat.gif'
+import logo from '../assets/JobTan.png'
 import { Link } from 'react-router-dom'
 import resume from '../assets/Resume.pdf'
 import './Navbar.css'
